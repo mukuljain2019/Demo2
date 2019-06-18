87f9165a4288371b58c810a3d430aec1df372f58
mukul <mukul.jain011@gmail.com>
2019-06-18 03:08:37 -0700

    test3 is added

---
+++ src/Test/GitHubTest3.java
@@ -0,0 +1,20 @@
+package Test;
+
+public class GitHubTest3 {
+
+	public static void main(String[] args)
+	{
+	
+	System.out.println("This is my Test3 in GitHub project");
+	System.out.println("This is my Test3 in GitHub project");
+	System.out.println("This is my Test3 in GitHub project");
+	
+	System.out.println("This is my Test3 in GitHub project");
+	System.out.println("This is my Test3 in GitHub project");
+	System.out.println("This is my Test3 in GitHub project");
+	
+	System.out.println("This is my Test3 in GitHub project");
+	System.out.println("This is my Test3 in GitHub project");
+	System.out.println("This is my Test3 in GitHub project");
+	}
+}
