250b04a0f6cce72695aa56f602e694d5b551d381
mukul <mukul.jain011@gmail.com>
2019-06-17 22:35:05 -0700

    reloaded

--- src/Test/WikiHubTest2.java
+++ src/Test/WikiHubTest2.java
@@ -5,10 +5,10 @@ public class WikiHubTest2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("This is my Second GitHub project");
-		System.out.println("This is my Second GitHub project");
-		System.out.println("This is my Second GitHub project");
-		System.out.println("This is my Second GitHub project");
+		System.out.println("This is my Second test in GitHub project");
+		System.out.println("This is my Second test GitHub project");
+		System.out.println("This is my Second test GitHub project");
+		System.out.println("This is my Second test GitHub project");
 		
 	}
 
